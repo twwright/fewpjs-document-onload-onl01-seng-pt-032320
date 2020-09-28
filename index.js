d216cb083409bff0c0d152bc84b6bd0b501f22d4
Thomas Wright <twwright@gmail.com>
2020-09-28 20:44:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", 
   function() { 
-    document.getElementById("text").innerHTML ="This is really cool!"; 
+    document.getElementById("text").innerHTML = "This is really cool!"; 
   }
 );
 
